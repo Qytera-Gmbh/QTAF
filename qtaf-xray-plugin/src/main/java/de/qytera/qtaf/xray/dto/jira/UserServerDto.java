d2f4a8a31df35be22d132be893aaf37b69300dc0
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2023-05-09 14:13:10 +0200

    Add `name` field to Jira server user DTO

--- qtaf-xray-plugin/src/main/java/de/qytera/qtaf/xray/dto/jira/UserServerDto.java
+++ qtaf-xray-plugin/src/main/java/de/qytera/qtaf/xray/dto/jira/UserServerDto.java
@@ -4,16 +4,15 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
- * The request for Jira Server when working with users.
+ * The DTO for Jira server when working with users.
  *
- * @see <a href="https://docs.atlassian.com/software/jira/docs/api/REST/9.8.0/#api/2/issue-assign">Assign (Jira Server)</a>
+ * @see <a href="https://docs.atlassian.com/software/jira/docs/api/REST/9.7.0/#api/2/user-getUser">Assign (Jira Server)</a>
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class UserServerDto extends UserDto<ApplicationRoleServerDto, GroupServerDto> {
-    /*
-     * Empty because Jira server does not add any additional or distinct fields to the abstract base class.
-     * Nonetheless, keeping the abstract super class feels like the right thing to do, as it keeps the cloud/server
-     * naming scheme consistent.
+    /**
+     * The username of the user.
      */
+    private String name;
 }
