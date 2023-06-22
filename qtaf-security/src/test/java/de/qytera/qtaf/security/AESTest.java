1cff6ca0daa44d8a0841496c358bf2fd93de4170
tflasche <tilo.flasche@qytera.de>
2023-06-22 15:37:56 +0200

    Fixed AES Test

--- qtaf-security/src/test/java/de/qytera/qtaf/security/AESTest.java
+++ qtaf-security/src/test/java/de/qytera/qtaf/security/AESTest.java
@@ -14,6 +14,7 @@ import java.util.HexFormat;
 public class AESTest {
     /**
      * Test if AES can encrypt a plain text and decrypt the ciphertext
+     *
      * @throws GeneralSecurityException Security Exception
      */
     @Test(testName = "Test AES encryption and decryption")
@@ -25,10 +26,11 @@ public class AESTest {
 
     /**
      * Test if AES can encrypt a plain text and decrypt the ciphertext
+     *
      * @throws GeneralSecurityException Security Exception
      */
-    @Test(testName = "Test AES encryption and decryption")
-    public void testAesEncryptionWithRandomSalts() throws GeneralSecurityException {
+    @Test(testName = "Test random salts being used in AES encryption and decryption")
+    public void testAesEncryptionRandomSalts() throws GeneralSecurityException {
         String cipherText1 = AES.encrypt("Hello World!", "my-key");
         String cipherText2 = AES.encrypt("Hello World!", "my-key");
         Assert.assertNotEquals(cipherText1, cipherText2, "ciphertext 1 and ciphertext 2 should be different, because different salts were used");
@@ -40,6 +42,7 @@ public class AESTest {
 
     /**
      * Test if AES can encrypt a plain text and decrypt the ciphertext
+     *
      * @throws GeneralSecurityException Security Exception
      */
     @Test(testName = "Test AES encryption and decryption with custom salt")
@@ -53,12 +56,13 @@ public class AESTest {
 
     /**
      * Test if ciphertext can only be decrypted with a valid key
+     *
      * @throws GeneralSecurityException Security exception
      */
     @Test(
             testName = "Test if ciphertext can only be decrypted with valid key",
             expectedExceptions = BadPaddingException.class,
-            expectedExceptionsMessageRegExp = "Given final block not properly padded. Such issues can arise if a bad key is used during decryption."
+            expectedExceptionsMessageRegExp = "(.+)Make sure you're using the correct key(.*)"
     )
     public void testCiphertextOnlyDecryptableWithValidKey() throws GeneralSecurityException {
         String cipherText = AES.encrypt("Hello World!", "my-key-1");
