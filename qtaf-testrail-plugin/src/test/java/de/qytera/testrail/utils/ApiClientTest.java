c6481112c21027fb7918e05b7e3e38f709a279a2
tflasche <tilo.flasche@qytera.de>
2023-06-22 17:25:54 +0200

    Added Test Cases for API Client

---
+++ qtaf-testrail-plugin/src/test/java/de/qytera/testrail/utils/ApiClientTest.java
@@ -0,0 +1,29 @@
+package de.qytera.testrail.utils;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * Testrail API client tests
+ */
+public class ApiClientTest {
+    @Test(description = "Test the constructor of the API client")
+    public void testApiClientConstructor() {
+        APIClient apiClient = new APIClient("https://inet.com");
+        Assert.assertEquals(apiClient.getM_url(), "https://inet.com/index.php?/api/v2/");
+    }
+
+    @Test(description = "Test the constructor of the API client")
+    public void testSendGetRequest() throws APIException, IOException {
+        APIClient apiClient = new APIClient("http://localhost:5000");
+        apiClient.sendGet("");
+    }
+
+    @Test(description = "Test the constructor of the API client")
+    public void testSendPostRequest() throws APIException, IOException {
+        APIClient apiClient = new APIClient("http://localhost:5000");
+        apiClient.sendPost("", new Object());
+    }
+}
