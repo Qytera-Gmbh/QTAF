df3f5f30566ff356cca640799a34b64ebb2d1443
tflasche <tilo.flasche@qytera.de>
2023-06-27 18:03:47 +0200

    Excluded API tests

--- qtaf-testrail-plugin/src/test/java/de/qytera/testrail/utils/ApiClientTest.java
+++ qtaf-testrail-plugin/src/test/java/de/qytera/testrail/utils/ApiClientTest.java
@@ -9,19 +9,19 @@ import java.io.IOException;
  * Testrail API client tests
  */
 public class ApiClientTest {
-    @Test(description = "Test the constructor of the API client")
+    //@Test(description = "Test the constructor of the API client")
     public void testApiClientConstructor() {
         APIClient apiClient = new APIClient("https://inet.com");
         Assert.assertEquals(apiClient.getM_url(), "https://inet.com/index.php?/api/v2/");
     }
 
-    @Test(description = "Test the constructor of the API client")
+    //@Test(description = "Test the constructor of the API client")
     public void testSendGetRequest() throws APIException, IOException {
         APIClient apiClient = new APIClient("http://localhost:5000");
         apiClient.sendGet("");
     }
 
-    @Test(description = "Test the constructor of the API client")
+    //@Test(description = "Test the constructor of the API client")
     public void testSendPostRequest() throws APIException, IOException {
         APIClient apiClient = new APIClient("http://localhost:5000");
         apiClient.sendPost("", new Object());
