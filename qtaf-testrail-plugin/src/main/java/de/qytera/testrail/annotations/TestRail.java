19335f0333282da352841f04715691ead5599952
tflasche <tilo.flasche@qytera.de>
2023-07-06 13:52:01 +0200

    Improved code based on code review

--- qtaf-testrail-plugin/src/main/java/de/qytera/testrail/annotations/TestRail.java
+++ qtaf-testrail-plugin/src/main/java/de/qytera/testrail/annotations/TestRail.java
@@ -8,7 +8,15 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface TestRail {
+    /**
+     * ID of the test case.
+     * @return test case ID
+     */
     String[] caseId() default "";
 
+    /**
+     * ID of a test run. It's like a test plan ID.
+     * @return test run ID
+     */
     String runId();
 }
