6838939ebdfe8e56e10308aea841a54ca6092d0d
tflasche <tilo.flasche@qytera.de>
2023-07-27 13:35:12 +0200

    Added test case

--- qtaf-aws-devicefarm/src/test/java/de/qytera/qtaf/aws_devicefarm/driver/DriverTest.java
+++ qtaf-aws-devicefarm/src/test/java/de/qytera/qtaf/aws_devicefarm/driver/DriverTest.java
@@ -1,7 +1,11 @@
 package de.qytera.qtaf.aws_devicefarm.driver;
 
 import com.amazonaws.SdkClientException;
+import de.qytera.qtaf.aws_devicefarm.config.AWSDeviceFarmConfigHelper;
+import de.qytera.qtaf.core.QtafFactory;
+import de.qytera.qtaf.core.config.entity.ConfigMap;
 import org.junit.Assert;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
@@ -34,5 +38,31 @@ public class DriverTest {
         Assert.assertNotNull(driver.getCapabilities());
         WebDriver webDriver = driver.getDriver();
         Assert.assertNotNull(webDriver);
+    }
+
+    @Test(expectedExceptions = {SdkClientException.class})
+    public void testAWSDriver() {
+        ConfigMap config = QtafFactory.getConfiguration();
+        String oldRegion = config.getString(AWSDeviceFarmConfigHelper.PROJECT_REGION);
+        config.setString(AWSDeviceFarmConfigHelper.PROJECT_REGION, "eu-central");
+        AbstractAWSDeviceFarmDriver driver = new AbstractAWSDeviceFarmDriver() {
+            @Override
+            public String getName() {
+                return "aws";
+            }
+
+            @Override
+            protected Capabilities getCapabilities() {
+                return null;
+            }
+
+            @Override
+            protected boolean isRemoteDriver() {
+                return true;
+            }
+        };
+
+        WebDriver d = driver.getDriver();
+        Assert.assertTrue(d instanceof WebDriver);
     }
 }
