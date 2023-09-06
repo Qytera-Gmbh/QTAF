b8f210f0288bfdd3e55ba800b4aea1846327558a
tflasche <tilo.flasche@qytera.de>
2023-09-06 14:40:01 +0200

    Added more test cases

--- qtaf-core/src/test/java/de/qytera/qtaf/core/log/TestScenarioLogCollectionTest.java
+++ qtaf-core/src/test/java/de/qytera/qtaf/core/log/TestScenarioLogCollectionTest.java
@@ -8,6 +8,8 @@ import de.qytera.qtaf.core.log.model.index.IndexHelper;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Parameter;
+
 public class TestScenarioLogCollectionTest {
     /**
      * Assert that two ScenarioLogCollection objects with the same ID are equal
@@ -100,4 +102,42 @@ public class TestScenarioLogCollectionTest {
         IndexHelper.clearAllIndices();
         Assert.assertEquals(TestScenarioLogCollection.getIndexSize(), 0);
     }
+
+    @Test
+    public void testAddParameters() throws NoSuchMethodException {
+        TestSuiteLogCollection slc = TestSuiteLogCollection.getInstance();
+        TestFeatureLogCollection fc1 = slc.createFeatureIfNotExists("feature1", "feature1");
+        TestScenarioLogCollection c1 = fc1.createScenarioIfNotExists(fc1.getFeatureId(), "scenario1", "instance1", "Scenario 1");
+        Assert.assertEquals(c1.getTestParameters().size(), 0);
+        Parameter[] params = DemoClass.class.getDeclaredMethod("foo", String.class, int.class).getParameters();
+        Object[] values = new Object[]{"Hello", 2};
+        c1.addParameters(params, values);
+        Assert.assertEquals(c1.getTestParameters().size(), 2);
+        Assert.assertEquals(c1.getTestParameters().get(0).getName(), "arg0");
+        Assert.assertEquals(c1.getTestParameters().get(0).getType(), "java.lang.String");
+        Assert.assertEquals(c1.getTestParameters().get(0).getValue(), "Hello");
+        Assert.assertEquals(c1.getTestParameters().get(1).getName(), "arg1");
+        Assert.assertEquals(c1.getTestParameters().get(1).getType(), "java.lang.Integer");
+        Assert.assertEquals(c1.getTestParameters().get(1).getValue(), 2);
+        // Clear up
+        slc.clearCollection();
+        IndexHelper.clearAllIndices();
+    }
+
+    @Test
+    public void testAddParameterValues() {
+        TestSuiteLogCollection slc = TestSuiteLogCollection.getInstance();
+        TestFeatureLogCollection fc1 = slc.createFeatureIfNotExists("feature1", "feature1");
+        TestScenarioLogCollection c1 = fc1.createScenarioIfNotExists(fc1.getFeatureId(), "scenario1", "instance1", "Scenario 1");
+        Assert.assertEquals(c1.getTestParameters().size(), 0);
+        c1.addParameters(new String[]{"Hello", "World"});
+        Assert.assertEquals(c1.getTestParameters().size(), 2);
+        // Clear up
+        slc.clearCollection();
+        IndexHelper.clearAllIndices();
+    }
+}
+
+class DemoClass {
+    public boolean foo(String arg1, int arg2) { return true; }
 }
