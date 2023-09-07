ab7101bb9ffaa582e80f4511645e8812a0a19b22
tflasche <tilo.flasche@qytera.de>
2023-09-07 15:02:54 +0200

    Improved JavaDoc based on stylechecker suggestions

--- qtaf-core/src/main/java/de/qytera/qtaf/testng/context/AssertionContext.java
+++ qtaf-core/src/main/java/de/qytera/qtaf/testng/context/AssertionContext.java
@@ -357,6 +357,7 @@ public interface AssertionContext {
 
     /**
      * Checks if two maps are deeply equal.
+     *
      * @param actual   The first object
      * @param expected The second object
      */
