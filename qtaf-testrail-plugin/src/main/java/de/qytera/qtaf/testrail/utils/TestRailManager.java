478f2cf450a8626323dff599465e41a5511841be
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2023-10-08 21:44:46 +0200

    Remove superfluous attachment headers

--- qtaf-testrail-plugin/src/main/java/de/qytera/qtaf/testrail/utils/TestRailManager.java
+++ qtaf-testrail-plugin/src/main/java/de/qytera/qtaf/testrail/utils/TestRailManager.java
@@ -3,23 +3,25 @@ package de.qytera.qtaf.testrail.utils;
 import com.google.common.net.HttpHeaders;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import de.qytera.qtaf.core.QtafFactory;
 import de.qytera.qtaf.core.gson.GsonFactory;
 import de.qytera.qtaf.http.RequestBuilder;
 import de.qytera.qtaf.http.WebService;
 import de.qytera.qtaf.testrail.entity.Attachments;
 import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.core.EntityPart;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPart;
+import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
 
 /**
  * A manager class for keeping track of and uploading executed tests.
@@ -60,19 +62,28 @@ public class TestRailManager {
      * @throws IOException  if the file cannot be accessed
      */
     public static void addAttachmentForTestCase(APIClient client, String testCaseId, String path) throws APIException, IOException {
-        File file = new File(path);
-        List<EntityPart> parts = new ArrayList<>();
-        parts.add(EntityPart.withName("attachment")
-                .fileName(file.getName())
-                .content(new FileInputStream(file))
-                .build());
-        RequestBuilder request = WebService.buildRequest(URI.create(client.getUrl() + "add_attachment_to_case/" + testCaseId));
-        request.getBuilder().header(HttpHeaders.AUTHORIZATION, client.getAuthorizationHeader());
-        try (Response response = WebService.post(request, Entity.entity(parts, MediaType.MULTIPART_FORM_DATA))) {
-            if (response.getStatus() != Response.Status.OK.getStatusCode()) {
-                String content = response.readEntity(String.class);
-                throw new APIException(response.getStatus(), content);
+        try (MultiPart multiPartEntity = new FormDataMultiPart()) {
+            FileDataBodyPart filePart = new FileDataBodyPart("attachment", new File(path));
+            // Testrail only accepts content-dispositions of the following form:
+            // Content-Disposition: form-data; name="attachment"; filename="..."
+            // See: https://github.com/gurock/testrail-api/blob/0320205d86f223415db3e8a301b9a534202ff820/java/com/gurock/testrail/APIClient.java#L166
+            filePart.setFormDataContentDisposition(new FormDataContentDisposition(
+                    "form-data; name=\"attachment\"; filename=\"%s\"".formatted(filePart.getFileEntity().getName()))
+            );
+            multiPartEntity.bodyPart(filePart);
+            RequestBuilder request = WebService.buildRequest(
+                    URI.create("%s/add_attachment_to_case/%s".formatted(client.getUrl(), testCaseId))
+            );
+            request.getBuilder().header(HttpHeaders.AUTHORIZATION, client.getAuthorizationHeader());
+            try (Response response = WebService.post(request, Entity.entity(multiPartEntity, MediaType.MULTIPART_FORM_DATA))) {
+                if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+                    String content = response.readEntity(String.class);
+                    throw new APIException(response.getStatus(), content);
+                }
             }
+        } catch (ParseException e) {
+            QtafFactory.getLogger().error("[QTAF TestRail Plugin] Failed to read attachment: %s", path);
+            QtafFactory.getLogger().error(e);
         }
     }
 
