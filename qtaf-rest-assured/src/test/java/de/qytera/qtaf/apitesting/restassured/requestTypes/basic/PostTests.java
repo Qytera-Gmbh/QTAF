aaff6fdd90394429c6ab62abc579293e51c3d9ed
Oliver Hönig <oliver-hoenig@gmx.de>
2024-02-19 16:36:10 +0100

    test(PostTest): add test cases for post request

--- qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/requestTypes/basic/PostTests.java
+++ qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/requestTypes/basic/PostTests.java
@@ -1,28 +1,91 @@
 package de.qytera.qtaf.apitesting.restassured.requestTypes.basic;
 
 import de.qytera.qtaf.apitesting.ApiTest;
+import de.qytera.qtaf.apitesting.log.model.message.ApiLogMessage;
 import de.qytera.qtaf.core.config.annotations.TestFeature;
+import de.qytera.qtaf.core.log.model.message.LogMessage;
 import de.qytera.qtaf.testng.context.QtafTestNGContext;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static de.qytera.qtaf.apitesting.ApiTestExecutor.apiTest;
 
 import static de.qytera.qtaf.apitesting.restassured.util.TestHelper.*;
 
-@TestFeature(name = "Body Assertion Tests", description = "Check the body assertion methods")
+@TestFeature(name = "POST Request Tests", description = "Check the post request methods")
 public class PostTests extends QtafTestNGContext implements ApiTest {
-    @Test(testName = "test")
-    public void test() {
-        String url = "https://jsonplaceholder.typicode.com";
+    String url = "https://reqres.in";
+    @Test(testName = "test postRequest() with wrong mediaFile-> PASSED")
+    public void testPostRequest_serverReceivesAnUnexpectedMediaFileType_PASSED() {
         apiTest(
                 this,
-                List.of(baseUri(url), basePath("/users/1")),
-                getRequest(),
                 List.of(
-                        statusCodeIsNot(404)
-                )
+                        baseUri(url),
+                        basePath("/api/users")),
+                postRequest(),
+                List.of()
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PASSED,
+                0,
+                ApiLogMessage.Action.RequestType.POST,
+                415
+        );
+    }
+
+    @Test(testName = "test postRequest() with right mediaFile-> PASSED")
+    public void testPostRequestPASSED() {
+        Map<String, String> jsonMap = new HashMap<>();
+        jsonMap.put("name", "morpheus");
+        jsonMap.put("job", "leader");
+
+        JSONObject jsonObject = new JSONObject(jsonMap);
+        apiTest(
+                this,
+                List.of(
+                        baseUri(url),
+                        basePath("/api/users"),
+                        json(jsonObject)
+                ),
+                postRequest(),
+                List.of()
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PASSED,
+                0,
+                ApiLogMessage.Action.RequestType.POST,
+                201
+        );
+    }
+
+    @Test(testName = "test postRequest() with wrong path but no assertion -> PASSED")
+    public void testPostRequestPASSEDWrongPathNoAssertionsPASSED() {
+        apiTest(
+                this,
+                List.of(
+                        baseUri(url),
+                        basePath("/user/1")),
+                postRequest(),
+                List.of()
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PASSED,
+                0,
+                ApiLogMessage.Action.RequestType.POST,
+                415
         );
     }
 }
