77e41a9d4f479390cb4eb8b954859317f6e03746
Oliver Hönig <oliver-hoenig@gmx.de>
2024-02-19 16:35:13 +0100

    test(HeadTest): add test cases for head request

--- qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/requestTypes/basic/HeadTests.java
+++ qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/requestTypes/basic/HeadTests.java
@@ -1,7 +1,9 @@
 package de.qytera.qtaf.apitesting.restassured.requestTypes.basic;
 
 import de.qytera.qtaf.apitesting.ApiTest;
+import de.qytera.qtaf.apitesting.log.model.message.ApiLogMessage;
 import de.qytera.qtaf.core.config.annotations.TestFeature;
+import de.qytera.qtaf.core.log.model.message.LogMessage;
 import de.qytera.qtaf.testng.context.QtafTestNGContext;
 import org.testng.annotations.Test;
 
@@ -10,18 +12,50 @@ import java.util.List;
 import static de.qytera.qtaf.apitesting.ApiTestExecutor.apiTest;
 
 import static de.qytera.qtaf.apitesting.restassured.util.TestHelper.*;
-@TestFeature(name = "Body Assertion Tests", description = "Check the body assertion methods")
+@TestFeature(name = "HEAD Request Tests", description = "Check the head request methods")
 public class HeadTests extends QtafTestNGContext implements ApiTest {
-    @Test(testName = "test")
-    public void test() {
-        String url = "https://jsonplaceholder.typicode.com";
+
+    String url = "https://jsonplaceholder.typicode.com";
+    @Test(testName = "test headRequest() -> PASSED")
+    public void testHeadRequestPASSED() {
+
         apiTest(
                 this,
-                List.of(baseUri(url), basePath("/users/1")),
-                getRequest(),
                 List.of(
-                        statusCodeIsNot(404)
-                )
+                        baseUri(url),
+                        basePath("/users/1")),
+                headRequest(),
+                List.of()
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PASSED,
+                0,
+                ApiLogMessage.Action.RequestType.HEAD,
+                200
+        );
+    }
+
+    @Test(testName = "test headRequest()with wrong path but no assertion -> PASSED")
+    public void testHeadRequestPASSEDWrongPathNoAssertionsPASSED() {
+        apiTest(
+                this,
+                List.of(
+                        baseUri(url),
+                        basePath("/user/1")),
+                headRequest(),
+                List.of()
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PASSED,
+                0,
+                ApiLogMessage.Action.RequestType.HEAD,
+                404
         );
     }
 }
