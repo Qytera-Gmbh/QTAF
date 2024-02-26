b49b84d7abc1398f37e48fab46a0ef81ec5adb60
Oliver Hönig <oliver-hoenig@gmx.de>
2024-02-26 11:41:50 +0100

    test(BodyTests): add body tests

--- qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/assertions/basic/BodyTests.java
+++ qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/assertions/basic/BodyTests.java
@@ -1,7 +1,10 @@
 package de.qytera.qtaf.apitesting.restassured.assertions.basic;
 
 import de.qytera.qtaf.apitesting.ApiTest;
+import de.qytera.qtaf.apitesting.log.model.message.ApiLogMessage;
 import de.qytera.qtaf.core.config.annotations.TestFeature;
+import de.qytera.qtaf.core.log.model.message.AssertionLogMessageType;
+import de.qytera.qtaf.core.log.model.message.LogMessage;
 import de.qytera.qtaf.testng.context.QtafTestNGContext;
 import org.testng.annotations.Test;
 
@@ -9,19 +12,41 @@ import java.util.List;
 
 import static de.qytera.qtaf.apitesting.ApiTestExecutor.apiTest;
 import static de.qytera.qtaf.apitesting.restassured.util.TestHelper.*;
+import static org.hamcrest.Matchers.*;
 
 @TestFeature(name = "Body Assertion Tests", description = "Check the body assertion methods")
 public class BodyTests extends QtafTestNGContext implements ApiTest {
-    @Test(testName = "test")
-    public void test() {
-        String url = "https://jsonplaceholder.typicode.com";
+    String url = "https://fakerestapi.azurewebsites.net/";
+    @Test(testName = "Test statusCodeIs(200) -> expect a logMessage that indicates PASSED")
+    public void testBody() {
         apiTest(
                 this,
-                List.of(baseUri(url), basePath("/users/1")),
+                List.of(
+                        baseUri(url),
+                        basePath("/api/v1/Books/{id}"),
+                        pathParam("id", 1)
+                ),
                 getRequest(),
                 List.of(
-                        statusCodeIsNot(404)
+                        body(hasKey("id"))
                 )
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "LogMessage",
+                latestApiLogMessage,
+                LogMessage.Status.FAILED,
+                1,
+                ApiLogMessage.Action.RequestType.GET,
+                200
+        );
+        apiAssertionMessageFitsTo(
+                "AssertionLogMessage 0",
+                getAssertionMessagesFormApiLogMessage(latestApiLogMessage).get(0),
+                AssertionLogMessageType.ASSERT_EQUALS,
+                hasKey("id"),
+                200,
+                LogMessage.Status.PASSED
         );
     }
 }
