ad4dc360b86c2849d97be41023837f2f545f05af
Oliver Hönig <oliver-hoenig@gmx.de>
2024-02-26 11:44:01 +0100

    test(RestAssuredTest): add a RESTassured test

--- qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/restAssured/RestAssuredTest.java
+++ qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/restAssured/RestAssuredTest.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 public class RestAssuredTest {
@@ -17,5 +18,26 @@ public class RestAssuredTest {
                 .get(url + "/user/1")
         .then()
                 .statusCode(404);
+    }
+
+    @Test
+    public void debugRestAssuredTest() {
+
+        given()
+                .baseUri("https://jsonplaceholder.typicode.com")
+                .when()
+                .get(url + "/users/1")
+                .then()
+                    .time(Matchers.lessThan(0L))
+                    .statusCode(200);
+                    //.time(Matchers.lessThan(10001L))
+                    //.statusCode(404);
+
+        /*
+                        responseTimeShouldBeLessThanXMilliseconds(0),
+                        statusCodeIs(200),
+                        responseTimeShouldBeLessThanXMilliseconds(10001),
+                        statusCodeIsNot(404)
+         */
     }
 }
