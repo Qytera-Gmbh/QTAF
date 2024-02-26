1cd66a5761f4f54f965ce39536e8672481363899
Oliver Hönig <oliver-hoenig@gmx.de>
2024-02-26 11:41:11 +0100

    test(TestHelper): add helper methods for testing
    Add the methods:
    - apiLogMessageUrlPathFitsTo()
    - getApiLogMessageAtIndexFromContext()
    
    to simplify test cases.

--- qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/util/TestHelper.java
+++ qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/util/TestHelper.java
@@ -6,9 +6,13 @@ import de.qytera.qtaf.core.context.IQtafTestContext;
 import de.qytera.qtaf.core.log.model.message.AssertionLogMessage;
 import de.qytera.qtaf.core.log.model.message.AssertionLogMessageType;
 import de.qytera.qtaf.core.log.model.message.LogMessage;
+import lombok.Getter;
+import lombok.Setter;
 import org.testng.Assert;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class TestHelper {
     public static List<LogMessage> getCurrentLogCollectionFrom(IQtafTestContext testContext) {
@@ -29,6 +33,21 @@ public class TestHelper {
 
     public static ApiLogMessage getLatestApiLogMessageFromContext(IQtafTestContext testContext){
         return getLatestApiLogMessageFromLogMessages(getCurrentLogCollectionFrom(testContext));
+    }
+
+    public static ApiLogMessage getApiLogMessageAtIndexFromContext(IQtafTestContext testContext, int index){
+        List<LogMessage> allLogMessages = getCurrentLogCollectionFrom(testContext);
+        List<ApiLogMessage> apiLogMessages = new ArrayList<>();
+        for (LogMessage logMessage : allLogMessages) {
+            ApiLogMessage convertedApiLogmessage = (logMessage instanceof ApiLogMessage ? (ApiLogMessage) logMessage : null);
+            if (convertedApiLogmessage != null) {
+                apiLogMessages.add(convertedApiLogmessage);
+            }
+        }
+        if (apiLogMessages.isEmpty()){
+            throw new IllegalArgumentException("The list of LogMessages provided does not include a ApiLogMessage");
+        }
+        return apiLogMessages.get(index);
     }
 
     public static void changeApiLogMessageStatusFromFailedToPassed(ApiLogMessage apiLogMessage){
@@ -65,5 +84,16 @@ public class TestHelper {
         Assert.assertEquals(apiLogMessage.getAssertions().size(), numberOfAssertions, message);
         Assert.assertEquals(apiLogMessage.getAction().getRequestType(), requestType, message);
         Assert.assertEquals(apiLogMessage.getResponse().getStatusCode(), statusCode, message);
+    }
+    public static void apiLogMessageUrlPathFitsTo(String message,
+                                           ApiLogMessage apiLogMessage,
+                                           String baseUri,
+                                           String basePath,
+                                           Map<String, ?> pathParams,
+                                           Map<String, ?> queryParams){
+        Assert.assertEquals(apiLogMessage.getRequest().getBaseUri(), baseUri, message + " <- from baseUri");
+        Assert.assertEquals(apiLogMessage.getRequest().getBasePath(), basePath, message + " <- from basePath");
+        Assert.assertEquals(apiLogMessage.getRequest().getPathParams(), pathParams, message + " <- from pathParams");
+        Assert.assertEquals(apiLogMessage.getRequest().getQueryParams(), queryParams, message + " <- from queryParams");
     }
 }
