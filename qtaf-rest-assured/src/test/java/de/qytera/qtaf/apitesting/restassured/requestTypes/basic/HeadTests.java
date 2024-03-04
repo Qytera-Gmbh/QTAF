8c17be0ac2e5942bc434062d7eee3e1f662cc853
Oliver Hönig <oliver-hoenig@gmx.de>
2024-03-04 11:28:24 +0100

    test(HeadTests): add test cases

--- qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/requestTypes/basic/HeadTests.java
+++ qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/requestTypes/basic/HeadTests.java
@@ -5,8 +5,10 @@ import de.qytera.qtaf.apitesting.log.model.message.ApiLogMessage;
 import de.qytera.qtaf.core.config.annotations.TestFeature;
 import de.qytera.qtaf.core.log.model.message.LogMessage;
 import de.qytera.qtaf.testng.context.QtafTestNGContext;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.net.ConnectException;
 import java.util.List;
 
 import static de.qytera.qtaf.apitesting.ApiTestExecutor.apiTest;
@@ -56,6 +58,31 @@ public class HeadTests extends QtafTestNGContext implements ApiTest {
                 0,
                 ApiLogMessage.Action.RequestType.HEAD,
                 404
+        );
+    }
+
+    @Test(testName = "test headRequest() not path -> PENDING")
+    public void testHeadRequestNoPathPASSED() {
+
+        try {
+            apiTest(
+                    this,
+                    List.of(),
+                    headRequest(),
+                    List.of()
+            );
+        } catch (Exception e){
+            Assert.assertTrue(e instanceof ConnectException);
+        }
+
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PENDING,
+                0,
+                ApiLogMessage.Action.RequestType.HEAD,
+                0
         );
     }
 }
