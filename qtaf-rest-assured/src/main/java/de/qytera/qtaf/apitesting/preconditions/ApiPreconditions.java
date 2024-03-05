878a54a4461eff65b325231bebe67a3eed685bb1
Oliver Hönig <oliver-hoenig@gmx.de>
2024-03-05 16:51:19 +0100

    refactor(ApiPreconditions): comments out unsupported methods for first release

--- qtaf-rest-assured/src/main/java/de/qytera/qtaf/apitesting/preconditions/ApiPreconditions.java
+++ qtaf-rest-assured/src/main/java/de/qytera/qtaf/apitesting/preconditions/ApiPreconditions.java
@@ -2,14 +2,11 @@ package de.qytera.qtaf.apitesting.preconditions;
 
 import de.qytera.qtaf.apitesting.log.model.message.ApiLogMessage;
 import io.restassured.http.*;
+import io.restassured.specification.MultiPartSpecification;
 import io.restassured.specification.RequestSpecification;
-import org.json.simple.JSONObject;
-import org.testng.Assert;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static de.qytera.qtaf.apitesting.log.model.message.ApiLogMessageUpdater.updateHeaderLogsByAddingAHeader;
@@ -31,7 +28,6 @@ public interface ApiPreconditions {
         };
     }
 
-
     default ApiPrecondition pathParam(String key, Object value) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.pathParam(key, value);
@@ -45,7 +41,6 @@ public interface ApiPreconditions {
         };
     }
 
-
     default ApiPrecondition pathParams(Map<String, Object> params) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.pathParams(params);
@@ -56,7 +51,6 @@ public interface ApiPreconditions {
             }
         };
     }
-
 
     default ApiPrecondition queryParam(String key, Object value) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
@@ -71,7 +65,6 @@ public interface ApiPreconditions {
         };
     }
 
-
     default ApiPrecondition queryParams(Map<String, Object> params) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.queryParams(params);
@@ -83,15 +76,13 @@ public interface ApiPreconditions {
         };
     }
 
-
-    default ApiPrecondition formParam(String key, String value) {
+    default ApiPrecondition formParam(String key, Object value) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.formParam(key, value);
 
             logMessage.getRequest().getFormParams().put(key, value);
         };
     }
-
 
     default ApiPrecondition formParams(Map<String, Object> params) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
@@ -103,8 +94,6 @@ public interface ApiPreconditions {
         };
     }
 
-
-
     default ApiPrecondition body(String body) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.body(body);
@@ -112,7 +101,6 @@ public interface ApiPreconditions {
             logMessage.getRequest().setBody(body);
         };
     }
-
 
     default ApiPrecondition body(Object object) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
@@ -122,7 +110,6 @@ public interface ApiPreconditions {
         };
     }
 
-
     default ApiPrecondition body(File file) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.body(file);
@@ -131,9 +118,7 @@ public interface ApiPreconditions {
         };
     }
 
-
     default ApiPrecondition contentType(String contentType) {
-        // TODO String as a parameter doesn't seem right
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.contentType(contentType);
 
@@ -142,7 +127,6 @@ public interface ApiPreconditions {
     }
 
     default ApiPrecondition contentType(ContentType contentType) {
-        // TODO String as a parameter doesn't seem right
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.contentType(contentType);
 
@@ -158,28 +142,6 @@ public interface ApiPreconditions {
         };
     }
 
-
-    default ApiPrecondition file(File file) {
-        return (RequestSpecification req, ApiLogMessage logMessage) -> {
-            req.contentType(ContentType.MULTIPART);
-            req.multiPart(file);
-
-            // TODO logMessage.getRequest().setFile(file);
-        };
-    }
-
-
-    default ApiPrecondition file(String s, File file) {
-        return (RequestSpecification req, ApiLogMessage logMessage) -> {
-            req.contentType(ContentType.MULTIPART);
-            req.multiPart(s, file);
-
-            logMessage.getRequest().setFileString(s);
-            // TODO logMessage.getRequest().setFile(file);
-        };
-    }
-
-
     default ApiPrecondition header(String key, String value) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.header(key, value);
@@ -189,7 +151,6 @@ public interface ApiPreconditions {
         };
     }
 
-
     default ApiPrecondition header(Header header) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.header(header);
@@ -197,7 +158,8 @@ public interface ApiPreconditions {
             updateHeaderLogsByAddingAHeader(logMessage, header);
         };
     }
-    /* TODO
+
+    /*
     @Step(name = "add headers", description = "add headers")
     default ApiTestRequestSpecification header(Headers headers) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
@@ -206,10 +168,7 @@ public interface ApiPreconditions {
             logMessage.getRequest().setHeaders(headers);
         };
     }
-
-     */
-
-    // RequestSpecification headers(Map<String, ?> var1);
+    */
 
     /*
     default ApiPrecondition headers(Map<String, ?> headers) {
@@ -219,11 +178,9 @@ public interface ApiPreconditions {
             logMessage.getRequest().setHeaders(headers);
         };
     }
-     */
+    */
 
-
-
-
+    /*
     default ApiPrecondition bearer(String bearerToken) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.header("Authorization", "Bearer " + bearerToken);
@@ -231,8 +188,9 @@ public interface ApiPreconditions {
             logMessage.getRequest().setBeareToken(bearerToken);
         };
     }
+     */
 
-
+    /*
     default ApiPrecondition cookie(String key, String value) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.cookie(key, value);
@@ -242,7 +200,9 @@ public interface ApiPreconditions {
 
         };
     }
+    */
 
+    /*
     default ApiPrecondition cookie(Cookie cookie) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.cookie(cookie);
@@ -250,7 +210,9 @@ public interface ApiPreconditions {
             logMessage.getRequest().setCookie(cookie);
         };
     }
+    */
 
+    /*
     default ApiPrecondition cookies(Cookies cookies) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.cookies(cookies);
@@ -258,7 +220,9 @@ public interface ApiPreconditions {
             logMessage.getRequest().setCookies(cookies);
         };
     }
+    */
 
+    /*
     default ApiPrecondition cookie(Map<String, ?> cookies) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.cookies(cookies);
@@ -266,7 +230,9 @@ public interface ApiPreconditions {
             logMessage.getRequest().setCookiesMap(cookies);
         };
     }
+    */
 
+    /*
     default ApiPrecondition json() {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.contentType(ContentType.JSON);
@@ -274,7 +240,9 @@ public interface ApiPreconditions {
             logMessage.getRequest().getContentType().add(ContentType.JSON.toString());
         };
     }
+    */
 
+    /*
     default ApiPrecondition json(JSONObject jsonObject) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.contentType(ContentType.JSON);
@@ -284,7 +252,20 @@ public interface ApiPreconditions {
             logMessage.getRequest().setBody(jsonObject.toString());
         };
     }
+    */
 
+    /*
+    default ApiPrecondition multiPart(MultiPartSpecification multiPartSpecification){
+        return (RequestSpecification req, ApiLogMessage logMessage) -> {
+            req.contentType(ContentType.MULTIPART);
+            req.multiPart(multiPartSpecification);
+
+            logMessage.getRequest().getContentType().add(ContentType.MULTIPART.toString());
+        };
+    };
+     */
+
+    /*
     default ApiPrecondition multipart() {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.contentType(ContentType.MULTIPART);
@@ -292,7 +273,34 @@ public interface ApiPreconditions {
             logMessage.getRequest().getContentType().add(ContentType.MULTIPART.toString());
         };
     }
+    */
+    /*
+    default ApiPrecondition multiPart(File file) {
+        return (RequestSpecification req, ApiLogMessage logMessage) -> {
+            req.contentType(ContentType.MULTIPART);
+            req.multiPart(file);
 
+            // logMessage.getRequest().setFile(file);
+        };
+    }
+     */
+
+
+    default ApiPrecondition multiPart(String s, File file) {
+        return (RequestSpecification req, ApiLogMessage logMessage) -> {
+            req.contentType(ContentType.MULTIPART);
+            req.multiPart(s, file);
+
+            logMessage.getRequest().setFileString(s);
+            // logMessage.getRequest().setFile(file);
+        };
+    }
+
+
+
+
+
+    /*
     default ApiPrecondition multipart(String s, Object o) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.contentType(ContentType.MULTIPART);
@@ -303,14 +311,18 @@ public interface ApiPreconditions {
             logMessage.getRequest().setMultipartObject(o);
         };
     }
+     */
 
+    /*
     default ApiPrecondition multipart(String s1, String s2) {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.contentType(ContentType.MULTIPART);
             req.multiPart(s1, s2);
         };
     }
+     */
 
+    /*
     default ApiPrecondition text() {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.contentType(ContentType.TEXT);
@@ -318,7 +330,9 @@ public interface ApiPreconditions {
             logMessage.getRequest().getContentType().add(ContentType.TEXT.toString());
         };
     }
+     */
 
+    /*
     default ApiPrecondition xml() {
         return (RequestSpecification req, ApiLogMessage logMessage) -> {
             req.contentType(ContentType.XML);
@@ -326,5 +340,6 @@ public interface ApiPreconditions {
             logMessage.getRequest().getContentType().add(ContentType.XML.toString());
         };
     }
+     */
 
 }
