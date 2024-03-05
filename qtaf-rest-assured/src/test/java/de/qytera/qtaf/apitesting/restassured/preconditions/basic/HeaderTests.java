18f6862b8efae0c8e673a069b37261f2c82b2834
Oliver Hönig <oliver-hoenig@gmx.de>
2024-03-05 11:54:41 +0100

    test(HeaderTest): add header tests

--- qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/preconditions/basic/HeaderTests.java
+++ qtaf-rest-assured/src/test/java/de/qytera/qtaf/apitesting/restassured/preconditions/basic/HeaderTests.java
@@ -7,14 +7,19 @@ import de.qytera.qtaf.core.log.model.message.AssertionLogMessageType;
 import de.qytera.qtaf.core.log.model.message.LogMessage;
 import de.qytera.qtaf.testng.context.QtafTestNGContext;
 import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import org.hamcrest.Matchers;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static de.qytera.qtaf.apitesting.ApiTestExecutor.apiTest;
+import static de.qytera.qtaf.apitesting.ApiTestExecutor.*;
 import static de.qytera.qtaf.apitesting.restassured.util.TestHelper.*;
 
 @TestFeature(name = "Header Precondition Tests", description = "Check the header precondition methods")
@@ -263,7 +268,46 @@ public class HeaderTests extends QtafTestNGContext implements ApiTest {
 
     // ====== Headers ======
 
-    @Test(testName = "test header(String, String) with multiple header values -> PASSED")
+    @Test(testName = "GIVEN: one header with header(String, String) -> WHEN: POST-request -> THEN: successful request and header is set to logMessage as expected")
+    public void testHeaderPASSED() {
+        Map<String, String> jsonMap = new HashMap<>();
+        jsonMap.put("name", "morpheus");
+        jsonMap.put("job", "leader");
+
+        JSONObject jsonObject = new JSONObject(jsonMap);
+        apiTest(
+                this,
+                specifyRequest(
+                        baseUri("https://reqres.in"),
+                        basePath("/api/users"),
+                        body(jsonObject),
+                        header("Content-Type", "application/json")
+                ),
+                postRequest(),
+                specifyAssertions(
+                        statusCodeIs(201)
+                )
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PASSED,
+                1,
+                ApiLogMessage.Action.RequestType.POST,
+                201
+        );
+        Header contentTypeHeader = new Header("Content-Type", "application/json");
+        Headers headers = new Headers(contentTypeHeader);
+
+        apiLogMessageHeadersFitsTo(
+                "",
+                latestApiLogMessage,
+                headers
+        );
+    }
+
+    @Test(testName = "GIVEN: multiple headers with header(String, String) -> WHEN: PUT-request -> THEN: successful request and headers get append to logMessage as expected")
     public void testMultipleHeaderPASSED() {
         File file = new File("src/test/java/de/qytera/qtaf/apitesting/restassured/preconditions/basic/assets/booking1.json");
         apiTest(
@@ -280,6 +324,104 @@ public class HeaderTests extends QtafTestNGContext implements ApiTest {
                 List.of(
                         statusCodeIs(200)
                 )
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PASSED,
+                1,
+                ApiLogMessage.Action.RequestType.PUT,
+                200
+        );
+        Header contentTypeHeader = new Header("Content-Type", "application/json");
+        Header acceptHeader = new Header("Accept", "application/json");
+        Header authHeader = new Header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=");
+
+        Headers headers = new Headers(contentTypeHeader,acceptHeader, authHeader);
+        apiLogMessageHeadersFitsTo(
+                "",
+                latestApiLogMessage,
+                headers
+        );
+    }
+
+    @Test(testName = "GIVEN: one header with header(Header header) -> WHEN: POST-request -> THEN: successful request and header is set to logMessage as expected")
+    public void testHeader2PASSED() {
+        Map<String, String> jsonMap = new HashMap<>();
+        jsonMap.put("name", "morpheus");
+        jsonMap.put("job", "leader");
+
+        JSONObject jsonObject = new JSONObject(jsonMap);
+
+        Header contentTypeHeader = new Header("Content-Type", "application/json");
+        apiTest(
+                this,
+                specifyRequest(
+                        baseUri("https://reqres.in"),
+                        basePath("/api/users"),
+                        body(jsonObject),
+                        header(contentTypeHeader)
+                ),
+                postRequest(),
+                specifyAssertions(
+                        statusCodeIs(201)
+                )
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PASSED,
+                1,
+                ApiLogMessage.Action.RequestType.POST,
+                201
+        );
+
+        Headers headers = new Headers(contentTypeHeader);
+
+        apiLogMessageHeadersFitsTo(
+                "",
+                latestApiLogMessage,
+                headers
+        );
+    }
+
+    @Test(testName = "GIVEN: multiple headers with header(Header header) -> WHEN: PUT-request -> THEN: successful request and headers get append to logMessage as expected")
+    public void testMultipleHeader2PASSED() {
+        File file = new File("src/test/java/de/qytera/qtaf/apitesting/restassured/preconditions/basic/assets/booking1.json");
+        Header contentTypeHeader = new Header("Content-Type", "application/json");
+        Header acceptHeader = new Header("Accept", "application/json");
+        Header authHeader = new Header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=");
+        apiTest(
+                this,
+                List.of(
+                        baseUri("https://restful-booker.herokuapp.com"),
+                        basePath("/booking/1"),
+                        header(contentTypeHeader),
+                        header(acceptHeader),
+                        header(authHeader),
+                        body(file)
+                ),
+                putRequest(),
+                List.of(
+                        statusCodeIs(200)
+                )
+        );
+        ApiLogMessage latestApiLogMessage = getLatestApiLogMessageFromContext(this);
+        apiLogMessageFitsTo(
+                "",
+                latestApiLogMessage,
+                LogMessage.Status.PASSED,
+                1,
+                ApiLogMessage.Action.RequestType.PUT,
+                200
+        );
+        Headers headers = new Headers(contentTypeHeader,acceptHeader, authHeader);
+        apiLogMessageHeadersFitsTo(
+                "",
+                latestApiLogMessage,
+                headers
         );
     }
 }
