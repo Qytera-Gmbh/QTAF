b7287dadba468d8d5566cee1a088c9e3c5ece7fe
tflasche <tilo.flasche@qytera.de>
2024-03-20 15:16:08 +0100

    Increased test coverage for XrayTestEntityBuilder

--- qtaf-xray-plugin/src/test/java/de/qytera/qtaf/xray/builder/XrayTestEntityBuilderTest.java
+++ qtaf-xray-plugin/src/test/java/de/qytera/qtaf/xray/builder/XrayTestEntityBuilderTest.java
@@ -10,6 +10,9 @@ import de.qytera.qtaf.xray.builder.test.MultipleIterationsXrayTestEntityBuilder;
 import de.qytera.qtaf.xray.builder.test.XrayTestEntityBuilder;
 import de.qytera.qtaf.xray.config.XrayConfigHelper;
 import de.qytera.qtaf.xray.entity.XrayTestEntity;
+import de.qytera.qtaf.xray.entity.XrayTestInfoEntityCloud;
+import de.qytera.qtaf.xray.entity.XrayTestInfoEntityServer;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.lang.annotation.Annotation;
@@ -53,17 +56,20 @@ public class XrayTestEntityBuilderTest {
 
     /**
      * This test case tests the building of the XrayTestEntity for the following configuration:
+     * - use xray cloud
      * - steps should not be merged
      * - test info should be updated
      */
     @Test
-    public void testBuildTestInfoStepEntities() throws NoSuchMethodException {
+    public void testBuildXrayCloudTestInfoStepEntities() throws NoSuchMethodException {
         // Get the QTAF configuration
         ConfigMap configMap = QtafFactory.getConfiguration();
 
         // Set merge steps value in config to false
+        String xrayServiceOldValue = configMap.getString(XrayConfigHelper.XRAY_SERVICE, "cloud");
         boolean mergeStepsOldValue = configMap.getBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_MERGE, false);
         boolean stepUpdateOOldValue = configMap.getBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_UPDATE, true);
+        configMap.setString(XrayConfigHelper.XRAY_SERVICE, "cloud");
         configMap.setBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_MERGE, false);
         configMap.setBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_UPDATE, true);
 
@@ -87,7 +93,59 @@ public class XrayTestEntityBuilderTest {
         // Build the xray upload entity for the dummy scenarios
         XrayTestEntity xrayTestEntity = builder.buildTestEntity(xrayTestAnnotation, sList);
 
+        // Test info entity should be built for Xray Cloud
+        Assert.assertTrue(xrayTestEntity.getTestInfo() instanceof XrayTestInfoEntityCloud);
+
         // Set merge steps value in config to old value
+        configMap.setString(XrayConfigHelper.XRAY_SERVICE, xrayServiceOldValue);
+        configMap.setBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_MERGE, mergeStepsOldValue);
+        configMap.setBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_UPDATE, stepUpdateOOldValue);
+    }
+
+    /**
+     * This test case tests the building of the XrayTestEntity for the following configuration:
+     * - use xray server
+     * - steps should not be merged
+     * - test info should be updated
+     */
+    @Test
+    public void testBuildXrayServerTestInfoStepEntities() throws NoSuchMethodException {
+        // Get the QTAF configuration
+        ConfigMap configMap = QtafFactory.getConfiguration();
+
+        // Set merge steps value in config to false
+        String xrayServiceOldValue = configMap.getString(XrayConfigHelper.XRAY_SERVICE, "cloud");
+        boolean mergeStepsOldValue = configMap.getBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_MERGE, false);
+        boolean stepUpdateOOldValue = configMap.getBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_UPDATE, true);
+        configMap.setString(XrayConfigHelper.XRAY_SERVICE, "server");
+        configMap.setBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_MERGE, false);
+        configMap.setBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_UPDATE, true);
+
+        // Get an annotation object needed for the test entity builder
+        XrayTest xrayTestAnnotation = DemoTest.class.getMethod("foo").getAnnotation(XrayTest.class);
+
+        // Get the suite log collection needed to the entity builder
+        TestSuiteLogCollection suiteLogCollection = QtafFactory.getTestSuiteLogCollection();
+
+        // The entity builder
+        XrayTestEntityBuilder<List<TestScenarioLogCollection>> builder = new MultipleIterationsXrayTestEntityBuilder(
+                suiteLogCollection,
+                new HashMap<>()
+        );
+
+        // Get dummy scenario log collections
+        TestScenarioLogCollection s1 = scenario(1, "scenario1", xrayTestAnnotation);
+        TestScenarioLogCollection s2 = scenario(1, "scenario2", xrayTestAnnotation);
+        List<TestScenarioLogCollection> sList = List.of(s1, s2);
+
+        // Build the xray upload entity for the dummy scenarios
+        XrayTestEntity xrayTestEntity = builder.buildTestEntity(xrayTestAnnotation, sList);
+
+        // Test info entity should be built for Xray Cloud
+        Assert.assertTrue(xrayTestEntity.getTestInfo() instanceof XrayTestInfoEntityServer);
+
+        // Set merge steps value in config to old value
+        configMap.setString(XrayConfigHelper.XRAY_SERVICE, xrayServiceOldValue);
         configMap.setBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_MERGE, mergeStepsOldValue);
         configMap.setBoolean(XrayConfigHelper.RESULTS_UPLOAD_TESTS_INFO_STEPS_UPDATE, stepUpdateOOldValue);
     }
